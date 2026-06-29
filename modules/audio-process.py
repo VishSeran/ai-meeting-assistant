bd008e58bb77b76cc73294b8cb62af3583c5f938
VishSeran <vishwa.sovis@gmail.com>
2026-06-29 13:59:39 +0530

    audio process - audio to text  updated

--- modules/audio-process.py
+++ modules/audio-process.py
@@ -5,18 +5,18 @@ from transformers import pipeline
 
 logger = get_logger("audio-process-logger")
 
-def download_audio (url, filename):
-    
+
+def download_audio(url, filename):
+
     try:
-        
         if not url:
             raise ValueError("Audio URL cannot be empty or none")
-        
+
         if not filename:
             raise ValueError("audio file name is empty or none")
-        
+
         response = requests.get(url=url)
-        
+
         if response.status_code == 200:
             logger.info("Response fetched from URL")
             with open(filename, "wb") as file:
@@ -26,24 +26,46 @@ def download_audio (url, filename):
         else:
             raise ValueError(f"Error in downloading audio: {response.status_code}")
         
+    except ValueError as e:
+        logger.error(f"Value error: {e}")
+        return None
+
     except Exception as e:
         logger.error(f"Error in download audio: {e}")
         return None
-    
+
+
+def audio_to_text(audio_path, model=STT_MODEL_NAME):
+
+    try:
+        if not audio_path:
+            raise ValueError("Audio file not found")
+
+        if not model:
+            raise ValueError("Speech to text model is empty or invalid")
+
+        pipe = stt_pipe 
+        
+        if stt_pipe:
+            logger.info("Speech to text pipeline is created")
+        
+        else:
+            raise ValueError("Pipeline error")
+        
+        text = stt_pipe(audio_path, batch_size = 8)["text"]
+        
+        if not text:
+            raise ValueError("Result text is empty or none")
+        else:
+            logger.info("Text extracted from audio is sucessfull")
+            return text
+        
     except ValueError as e:
         logger.error(f"Value error: {e}")
         return None
 
-def audio_to_text(audio, model=STT_MODEL_NAME):
-    
-    try:
-        
-        
     except Exception as e:
         logger.error(f"Error in audio to text: {e}")
         return None
-    
-    except ValueError as e:
-        logger.error(f"Value error: {e}")
-        return None 
 
+    
--- modules/config.py
+++ modules/config.py
@@ -1,1 +1,7 @@
+from transformers import pipeline
+
 STT_MODEL_NAME = "openai/whisper-tiny.en"
+
+stt_pipe = pipeline(
+    task="automatic-speech-recognition", model=STT_MODEL_NAME, chunk_length_s=30
+)
