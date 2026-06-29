72b6aa8de40739a66012e4bb1f3d9c377f15fe36
VishSeran <vishwa.sovis@gmail.com>
2026-06-29 12:38:32 +0530

    audio process - download audio fb updated

--- modules/audio-process.py
+++ modules/audio-process.py
@@ -1,4 +1,32 @@
 from modules.logger import get_logger
-
+import requests
 
 logger = get_logger("audio-process-logger")
+
+def download_audio (url, filename):
+    
+    try:
+        
+        if not url:
+            raise ValueError("Audio URL cannot be empty or none")
+        
+        if not filename:
+            raise ValueError("audio file name is empty or none")
+        
+        response = requests.get(url=url)
+        
+        if response.status_code == 200:
+            logger.info("Response fetched from URL")
+            with open(filename, "wb") as file:
+                file.write(response.content)
+                logger.info("Audio file has created")
+        else:
+            raise ValueError(f"Error in downloading audio: {response.status_code}")
+    except Exception as e:
+        logger.error(f"Error in download audio: {e}")
+        return None
+    
+    except ValueError as e:
+        logger.error(f"Value error: {e}")
+        return None
+
