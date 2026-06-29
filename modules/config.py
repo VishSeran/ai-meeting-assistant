4184dfd3e863e991780331edf3a5436fc5f6e698
VishSeran <vishwa.sovis@gmail.com>
2026-06-29 14:31:47 +0530

    model-process- policy assistant updated

--- modules/config.py
+++ modules/config.py
@@ -1,6 +1,7 @@
 from transformers import pipeline
 
 STT_MODEL_NAME = "openai/whisper-tiny.en"
+LLM_MODEL = "meta-llama/llama-3-2-11b-vision-instruct"
 
 STT_PIPE = pipeline(
     task="automatic-speech-recognition", model=STT_MODEL_NAME, chunk_length_s=30
